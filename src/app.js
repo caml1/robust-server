0e25e255e1b1dc36c1bb8f731b1c100b3863893c
Juan Garcia <juangarcia@Juans-MacBook-Air.local>
2024-07-09 17:20:28 -0400

    Added 2 routs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
-
 const app = express();
+const urls = require("./data/urls-data")
 
 app.use(express.json());
+
+app.get('/urls', (req, res) => {
+    res.json({ data: urls });
+})
+app.get('/urls/:urlId', (req, res) => {
+    const { urlId } = req.params;
+    const foundUrl = urls.find(url => url.id === Number(urlId));
+
+    res.json({ data: foundUrl })
+})
 
 // TODO: Add code to meet the requirements and make the tests pass.
 
--- src/data/urls-data.js
+++ src/data/urls-data.js
@@ -3,4 +3,8 @@ module.exports = [
     href: "http://www.google.com",
     id: 1,
   },
+  {
+    href: "http://www.facebook.com",
+    id: 2,
+  },
 ];
